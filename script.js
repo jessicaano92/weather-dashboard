2865abbf5d631156bf0afbe35cb4df7bf0102f35
Jessica Anocibar <jessicayaws@yahoo.com>
2020-10-07 19:26:09 -0600

    trying to fix js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 
 
-var APIkey = "0ba0c1d4b48af80e63d116478eae89cc"
+var APIkey = "0ba0c1d4b48af80e63d116478eae89cc";
+// var testCity = "Denver";
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
+
+
+// function showWeather(e) {
+//   $.ajax({
+//     method: "GET",
+//     url: "https://api.openweathermap.org/data/2.5/weather?q=" + testCity + APIkey,
+//   }).then(function(response){
+//     console.log(response);
+//   });
+// } 
+// showWeather();
 
 
 $(document).ready(function() {
@@ -81,6 +94,8 @@ function getCity(searchCity) {
   })        
 
 }
+       
+
 
         function getForecast(searchCity) {
           
@@ -94,15 +109,17 @@ function getCity(searchCity) {
             $("#forecast").html("<h4 class='mt-3'> 5-Day Forecast:</h4>").append("<div class=\"row\">");
                 for (i = 0; i < data.list.length; i++){
                     if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                      
                       var col = $("<div>").addClass("col-md-2");
                       var card = $("<div>").addClass("card bg-primary text-white");
-                      var body = $("<div>").addClass("card-body p-2")
-                      var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png" )
-                      var p1 = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp_max)
-                      var p2 = $("<p>").addClass("card-text").text("Humidity " + data.list[i].main.humidity)
+                      var body = $("<div>").addClass("card-body p-2");
+                      var title = $("<h5>").addClass("card-title").text(new Date(data.list[i].dt_txt.toLocalDateString()));
+                      var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png" );
+                      var p1 = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp_max + ' F');
+                      var p2 = $("<p>").addClass("card-text").text("Humidity " + data.list[i].main.humidity);
 
 
-                      col.append(card.append(body.append(img, p1, p2)));
+                      col.append(card.append(body.append(title, img, p1, p2)));
                       $("#forecast .row").append(col);
                     }
                 }
